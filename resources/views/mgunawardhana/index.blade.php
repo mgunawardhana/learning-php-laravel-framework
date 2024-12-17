7179786bd7b1fa6a9a3f9dfc7552523942979610
mgunawardhana <mrgunawardhana27368@gmail.com>
2024-12-17 21:45:04 +0530

    making first routes

---
+++ resources/views/mgunawardhana/index.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>mGunawardhana's Network</title>
+</head>
+<body>
+<h2> Currently Available mGunawardhana's</h2>
+<ul>
+    <li>Ninjas Here</li>
+</ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/mgunwardhanas',function(){
+    return view('mgunawardhana.index');
+});
